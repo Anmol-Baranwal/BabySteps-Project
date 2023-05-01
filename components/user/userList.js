ad5efac21d3cafa205586dcfb1684a990b5a508a
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-01 23:27:26 +0530

    add props data for rendering content

--- components/user/userList.js
+++ components/user/userList.js
@@ -1,10 +1,22 @@
-const userList = () => {
+function UserList() {
+  const [users, setUsers] = useState([]);
+
   return (
     <div>
-      <h1>hi</h1>
-      <p>hi</p>
+      <h1>User List</h1>
+      {users.map((user) => {
+        // we can also use: ({ id, name, username, email, phone, website, companyName }) => {
+        <div key={user.id}>
+          <h2>{user.name}</h2>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <p>Phone: {user.phone}</p>
+          <p>Website: {user.website}</p>
+          <p>Company Name: {user.company.name}</p>
+        </div>;
+      })}
     </div>
   );
-};
+}
 
 export default userList;
