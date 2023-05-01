a4de635f633d5e37b81e7dc985b43807f12de3de
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-01 19:12:36 +0530

    remove unwanted code, and add poppins config font
    change directory to root

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>waitSmart: Smart Solution</title>
+        <meta name="description" content="Stop waiting in queues" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Hi</div>
+    </>
+  );
+}
