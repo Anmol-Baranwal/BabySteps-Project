7a941ac21f945bd250ff3d04221347aeecb8f331
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-01 23:39:47 +0530

    fetch the users based on mounting the component

--- components/user/userList.js
+++ components/user/userList.js
@@ -1,5 +1,39 @@
+import { useEffect } from "react";
+
 function UserList() {
   const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = res.json();
+        const users = data.map(
+          ({
+            id,
+            name,
+            username,
+            email,
+            phone,
+            website,
+            company: { name: companyName },
+          }) => ({
+            id,
+            name,
+            username,
+            email,
+            phone,
+            website,
+            companyName,
+          })
+        );
+        setUsers(users);
+      } catch (err) {
+        console.log("error in fetching users" + err);
+      }
+    }
+    fetchUsers();
+  }, []); // When this list is empty ([]), it means that the effect will only run once when the component mounts, and never again.
 
   return (
     <div>
