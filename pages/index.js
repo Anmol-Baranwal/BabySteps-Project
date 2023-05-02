fe7af364a29aab33eaf60e09e19481ebe1c5f7aa
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-02 13:01:36 +0530

    update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,19 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import UserList from "@/components/user/userList";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>waitSmart: Smart Solution</title>
+        <title>Information of Users</title>
         <meta name="description" content="Stop waiting in queues" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>Hi</div>
+      <div>
+        <UserList />
+      </div>
     </>
   );
 }
