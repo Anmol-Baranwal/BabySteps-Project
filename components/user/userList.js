6a89ad1582b7f731975f9d0dc6f7a68f4140d536
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-02 23:14:08 +0530

    add loading state with minor styling

--- components/user/userList.js
+++ components/user/userList.js
@@ -5,6 +5,7 @@ import styles from "./userList.module.css";
 
 function UserList() {
   const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true); // add loading state
 
   console.log({ users });
 
@@ -34,6 +35,7 @@ function UserList() {
         );
         console.log({ users });
         setUsers(users);
+        setLoading(false); // since data is fetched
       } catch (err) {
         console.log("error in fetching users" + err);
       }
@@ -44,36 +46,40 @@ function UserList() {
   return (
     <div className={styles.main}>
       <h1 className={styles.heading}>User List</h1>
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <th className={styles.tableheading}>Name</th>
-            <th className={styles.tableheading}>Username</th>
-            <th className={styles.tableheading}>Email</th>
-            <th className={styles.tableheading}>Phone</th>
-            <th className={styles.tableheading}>Website</th>
-            <th className={styles.tableheading}>Company Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => {
-            // we can also use: ({ id, name, username, email, phone, website, companyName }) => {
-            return (
-              <tr key={user.id} className={styles.userBlock}>
-                <Link href={`/user/${user.id}`} className={styles.customLink}>
-                  {/* <td className={cls(styles.name, styles.tablecell)}> */}
-                  <td className={styles.name}>{user.name}</td>
-                </Link>
-                <td className={styles.tablecell}>{user.username}</td>
-                <td className={styles.tablecell}>{user.email}</td>
-                <td className={styles.tablecell}>{user.phone}</td>
-                <td className={styles.tablecell}>{user.website}</td>
-                <td className={styles.tablecell}>{user.companyName}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      {loading ? ( // check if loading is true or false
+        <p className={styles.loading}>Loading users...</p>
+      ) : (
+        <table className={styles.table}>
+          <thead>
+            <tr>
+              <th className={styles.tableheading}>Name</th>
+              <th className={styles.tableheading}>Username</th>
+              <th className={styles.tableheading}>Email</th>
+              <th className={styles.tableheading}>Phone</th>
+              <th className={styles.tableheading}>Website</th>
+              <th className={styles.tableheading}>Company Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => {
+              // we can also use: ({ id, name, username, email, phone, website, companyName }) => {
+              return (
+                <tr key={user.id} className={styles.userBlock}>
+                  <Link href={`/user/${user.id}`} className={styles.customLink}>
+                    {/* <td className={cls(styles.name, styles.tablecell)}> */}
+                    <td className={styles.name}>{user.name}</td>
+                  </Link>
+                  <td className={styles.tablecell}>{user.username}</td>
+                  <td className={styles.tablecell}>{user.email}</td>
+                  <td className={styles.tablecell}>{user.phone}</td>
+                  <td className={styles.tablecell}>{user.website}</td>
+                  <td className={styles.tablecell}>{user.companyName}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 }
