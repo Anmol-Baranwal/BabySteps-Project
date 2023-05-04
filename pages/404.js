68baf87c94f065cef422834ad06bd72da4aef0fb
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-04 14:35:15 +0530

    change image name for better readability

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ const ErrorPage = () => {
       <div className={styles.content}>User ID valid till 10</div>
       <div className={styles.imageWrapper}>
         <Image
-          src="/static/4.png"
+          src="/static/404.png"
           alt="404 page custom illustration"
           width={2500 / 3.4}
           height={1778 / 3.4}
