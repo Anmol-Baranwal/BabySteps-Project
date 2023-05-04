88f1beff7e87d98b381bfc884f3596ca691439c9
Anmol Baranwal <74038190+Anmol-Baranwal@users.noreply.github.com>
2023-05-04 10:29:53 +0530

    add custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "@/styles/404.module.css";
+
+const ErrorPage = () => {
+  return (
+    <div className={styles.main}>
+      <div className={styles.content}>User ID valid till 10</div>
+      <div className={styles.imageWrapper}>
+        <Image
+          src="/static/4.png"
+          alt="404 page custom illustration"
+          width={2500 / 3.4}
+          height={1778 / 3.4}
+          className={styles.customImage}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
